265ccfb76d03807a859ed14d3d23207e65b3c893
peterson oaikhenah <oaikhenahpeterson@gmail.com>
2021-04-26 03:08:36 +0100

    update auth url path and code comments

--- devcentertest/urls.py
+++ devcentertest/urls.py
@@ -5,9 +5,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 from api.views import UserViewSet
 
 # base project url patterns
+# django provides an easy to use router for all your apps
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/<version>/', include('api.urls')),
+    # django provides a default usermodel
     path('api/<version>/auth/login/', obtain_auth_token),
     path('api/<version>/auth/signup/', UserViewSet)
 
